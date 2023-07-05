da8098c7addcb6b2ddb403c2fc3421d250258369
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2023-07-05 12:38:26 +0200

    pk not null

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
-drop table if exists Datacenter;
-drop table if exists Host;
-drop table if exists Software;
 drop table if exists Application;
 drop table if exists Ticket;
 drop table if exists User;
+drop table if exists Software;
+drop table if exists Host;
+drop table if exists Datacenter;
 
 create table Datacenter (
-  id text primary key, 
+  id text primary key not null, 
   address text
 );
 
 create table Host (
-  id text primary key,
+  id text primary key not null,
   os text,
   cpus int,
   ramGB int,
@@ -21,25 +21,25 @@ create table Host (
 );
 
 create table Software (
-  id text primary key,
+  id text primary key not null,
   vendor text,
   name text,
   version text
 );
 
 create table Application (
-  id text primary key,
+  id text primary key not null,
   software text references Software(id),
   runsOn text references Host(id)
 );
 
 create table User (
-  id text primary key,
+  id text primary key not null,
   name text
 );
 
 create table Ticket (
-  id text primary key,
+  id text primary key not null,
   description text,
   created date,
   user text references User(id),
