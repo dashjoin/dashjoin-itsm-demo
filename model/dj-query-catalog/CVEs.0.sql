8867a3578f328a41c564ae65d085c9935ec2240e
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2023-07-06 12:24:14 +0200

    improve query

--- model/dj-query-catalog/CVEs.0.sql
+++ model/dj-query-catalog/CVEs.0.sql
@@ -1,1 +1,9 @@
-select * from Software, vulnerability where Software.vendor = vulnerability.vendor and Software.name = vulnerability.software
+select 
+  Application.software, Application.description, Software.vendor, Software.name, cve.id, cve.impactScore, cve.url
+from 
+  Application, Software, vulnerability, cve
+where 
+  Application.software = Software.id and
+  Software.vendor = vulnerability.vendor and Software.name = vulnerability.software and 
+  Software.id <> 'linux_kernel' and
+  vulnerability.cve = cve.id
